38ff143e84fe5c4f0cb3b5b6dd03a41a515fd34e
“Olaf” <olaf@snowswap.org>
2021-03-04 16:18:15 -0700

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,51 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-vyper");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/ULzKnrrt6iflDEREoi2qNJzqRL6N6y6X",
+      },
+    },
+  },
+  vyper: {
+    version: "0.2.8",
+  },
+  solidity: {
+    version: "0.5.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,66 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const LpToken = await hre.ethers.getContractFactory("LpTokenV3");
+  const lp = await LpToken.deploy("Snow usdc/fusdc/fusdt/fdai", "usdc/fusdc/fusdt/fdai");
+
+  const lpContract = await lp.deployed();
+  // await lo.mint('0xe5c382532179e5086ef8e862c6e13d331a3618da', hre.ethers.BigNumber.from('2000000000000000000000'));
+
+  console.log("Deployed Lp Token Contract: ", lpContract.address);
+
+  const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+  const FUSDC = "0xc3f7ffb5d5869b3ade9448d094d81b0521e8326f";
+  const FUSDT = "0xc7ee21406bb581e741fbb8b21f213188433d9f2f";
+  const FDAI = "0xe85c8581e60d7cd32bbfd86303d2a4fa6a951dac";
+
+  const _A = 70;
+  const _fee = 4000000;
+  const _admin_fee = 0;
+  // _owner: address,
+  // _coins: address[N_COINS],
+  // _pool_token: address,
+  // _A: uint256,
+  // _fee: uint256,
+  // _admin_fee: uint256
+
+  const StableSwap = await hre.ethers.getContractFactory("StableSwapV3");
+  const stableSwap = await StableSwap.deploy(
+    "0xE8fed9d7b9E7eD19671ee35f169db6F007b2FFd4",
+    [USDC, FUSDC, FUSDT, FDAI],
+    lpContract.address,
+    _A,
+    _fee,
+    _admin_fee
+  );
+
+  const stableSwapContract = await stableSwap.deployed();
+  console.log("StableSwap contract has been deployed: ", stableSwapContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
