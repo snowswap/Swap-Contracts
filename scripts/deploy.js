00c504b7c8a632153544433bc2e6f9b1944e9af6
“Olaf” <olaf@snowswap.org>
2021-03-04 16:36:17 -0700

    Set minter

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,7 @@
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
@@ -15,14 +16,14 @@ async function main() {
 
   // We get the contract to deploy
 
-  const [deployer] = await ethers.getSigners();
+  const [owner, addr1] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Deploying contracts with the account:", owner.address);
 
-  const LpToken = await hre.ethers.getContractFactory("LpTokenV3");
-  const lp = await LpToken.deploy("Snow usdc/fusdc/fusdt/fdai", "usdc/fusdc/fusdt/fdai");
+  const lpContractArtifact = await hre.ethers.getContractFactory("LpTokenV3");
+  const lpContract = await lpContractArtifact.deploy("Snow usdc/fusdc/fusdt/fdai", "usdc/fusdc/fusdt/fdai");
 
-  const lpContract = await lp.deployed();
+  await lpContract.deployed();
   // await lo.mint('0xe5c382532179e5086ef8e862c6e13d331a3618da', hre.ethers.BigNumber.from('2000000000000000000000'));
 
   console.log("Deployed Lp Token Contract: ", lpContract.address);
@@ -31,7 +32,6 @@ async function main() {
   const FUSDC = "0xc3f7ffb5d5869b3ade9448d094d81b0521e8326f";
   const FUSDT = "0xc7ee21406bb581e741fbb8b21f213188433d9f2f";
   const FDAI = "0xe85c8581e60d7cd32bbfd86303d2a4fa6a951dac";
-
   const _A = 70;
   const _fee = 4000000;
   const _admin_fee = 0;
@@ -42,8 +42,8 @@ async function main() {
   // _fee: uint256,
   // _admin_fee: uint256
 
-  const StableSwap = await hre.ethers.getContractFactory("StableSwapV3");
-  const stableSwap = await StableSwap.deploy(
+  const StableSwapArtifact = await hre.ethers.getContractFactory("StableSwapV3");
+  const stableSwapContract = await StableSwapArtifact.deploy(
     "0xE8fed9d7b9E7eD19671ee35f169db6F007b2FFd4",
     [USDC, FUSDC, FUSDT, FDAI],
     lpContract.address,
@@ -52,8 +52,14 @@ async function main() {
     _admin_fee
   );
 
-  const stableSwapContract = await stableSwap.deployed();
+  await stableSwap.deployed();
   console.log("StableSwap contract has been deployed: ", stableSwapContract.address);
+
+  // @notice: Set the swap contract address as minter on lp contract
+  //          So that swap contract can mint the lp tokens
+  await lpContract.set_minter(stableSwapContract.address);
+
+  console.log("Minter address has been set!");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
